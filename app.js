5eb4f8ea105c3353778782933dda995094ca83aa
unknown <ayushkhandelwal227@gamil.com>
2023-06-30 22:25:39 +0530

    Added the hashing method of encryption in code

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
  
-console.log(process.env.API_KEY);
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +19,6 @@ const userSchema = new mongoose.Schema ({
     password : String
 });
 
-userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -40,7 +37,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     });
     newUser.save()
     .then(function(){
@@ -53,7 +50,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username })
   .then(function(foundUser) {
