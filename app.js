ee48677776edee13611534d2eec8f33efc9f0176
unknown <ayushkhandelwal227@gamil.com>
2023-07-03 04:46:36 +0530

    In these code update i use passport strategy for google oAuth2.0

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate = require("mongoose-findOrCreate");
 
 const app = express();
  
@@ -27,22 +29,56 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser : true});
 
 const userSchema = new mongoose.Schema ({
     email: String,
-    password : String
+    password : String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     res.render("home");
 });
- 
+
+app.route('/auth/google')
+  .get(passport.authenticate('google', {
+    scope: ['profile']
+  })); 
+
+  app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login"}),
+  function(req, res){
+    res.redirect("/secrets");
+  }
+  )
+
+
 app.get("/login",function(req,res){
     res.render("login");
 });
